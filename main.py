7e136f4cf5c4cfbd4d1a380916604a6af62c9f14
Your Name <baobabkiki@gmail.com>
2021-11-24 12:32:06 +0000

    Serialise and deserialise

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import sys
 import os
 from math import pi
+import yaml
 
 class Circle:
 
-    def __init__(self, radius, fill='red', stroke='black'):
+    def __init__(self, radius, fill='red', stroke='black', at=(0,0)):
         self._radius = radius
         self._fill = fill
         self._stroke = stroke
+        self._at = at
 
     @property
     def radius(self): #public access for ._radius; read-only
@@ -26,9 +28,32 @@ class Circle:
     def __repr__(self):
         return f"Circle({self._radius}, fill={self._fill}, stroke={self._stroke})"
 
+    def __str__(self):
+        string = yaml.dump({
+            'circle' : {
+                'radius': self._radius,
+                'fill': self._fill,
+                'stroke': self._stroke,
+                'at': self._at
+            }
+        })
+        return string
+
+    @classmethod
+    def from_yaml(cls, string):
+        """Creatr a circle from YAML instance"""
+        circle_dict = yaml.load(string, Loader=yaml.Loader)['circle']
+        print(circle_dict)
+        obj = cls(circle_dict['radius'], fill=circle_dict['fill'], stroke=circle_dict['stroke'], at=circle_dict['at'])
+        return obj
+
+
+
+
+
 class Quadrilateral:
 
-    def __init__(self, width, height, fill= "red", stroke="black"):
+    def __init__(self, width, height, fill="red", stroke="black"):
         self._width = width
         self._height = height
         self._fill = fill
@@ -59,6 +84,26 @@ def main():
     print(circle())
     print(repr(circle))
     print(quadrilateral.calculate_area())
+
+    my_dict = {
+        'key':{
+            'inside_dict':[5,6,7,8]
+        }
+    }
+    my_yaml= yaml.dump(my_dict)
+    print(my_yaml)
+    print(circle)
+
+    yaml_circle = """\
+circle:
+  at: !!python/tuple
+  - 0
+  - 0
+  fill: orange
+  radius: 5.0
+  stroke: red """
+    my_circle = Circle.from_yaml(yaml_circle)
+
     return os.EX_OK
 
 
