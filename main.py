df8b77724da0dd427114e320b37db03697086a23
Your Name <baobabkiki@gmail.com>
2021-11-24 11:52:24 +0000

    Quadrilateral, canvas and text class defined

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 from math import pi
 
 class Circle:
+
     def __init__(self, radius, fill='red', stroke='black'):
         self._radius = radius
-        self._fill= fill
-        self._stroke= stroke
+        self._fill = fill
+        self._stroke = stroke
 
     @property
     def radius(self): #public access for ._radius; read-only
@@ -22,12 +23,47 @@ class Circle:
     def __call__(self):
         return "I am a circle"
 
+    def __repr__(self):
+        return f"Circle({self._radius}, fill={self._fill}, stroke={self._stroke})"
+
+class Quadrilateral:
+
+    def __init__(self, width, height, fill= "red", stroke="black"):
+        self._width = width
+        self._height = height
+        self._fill = fill
+        self._stroke = stroke
+
+    def calculate_area(self):
+        return self._width * self._height
+
+class Canvas:
+
+    def __init__(self, width, height, bg="white"):
+        self._width = width
+        self._height = height
+        self.background = bg
+
+class Text:
+    def __init__(self, size, color="black", font="Times"):
+        self._color = color
+        self._size = size
+        self._font = font
+
+
 def main():
-    circle= Circle(5.0, fill='orange', stroke='red')
+    circle = Circle(5.0, fill='orange', stroke='red')
+    quadrilateral = Quadrilateral(5, 10547, fill='orange', stroke='red')
     print(f"area= {circle.calculate_area()}")
     print(f"circumference is {len(circle)}")
     print(circle())
+    print(repr(circle))
+    print(quadrilateral.calculate_area())
     return os.EX_OK
+
+
+
+
 
 if __name__ == "__main__":
     sys.exit(main())
